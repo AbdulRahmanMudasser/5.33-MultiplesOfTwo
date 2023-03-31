6b5b09ccb635d3f49ef7108e34673b2c8ebae50e
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 17:51:59 +0500

    solution done

--- MultiplesOfTwo/Program.cs
+++ MultiplesOfTwo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Metrics;
+
+class MultiplesOfTwo
+{
+    static void Main(string[] args)
+    {
+        // declaring variables
+        long number = 1;
+        int counter = 0;
+
+        while (counter != 40)
+        {
+            Console.Write((number *= 2) + " ");
+
+            counter++;
+        }
+    }
+}
